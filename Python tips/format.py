2b9d90722cd09868b02288d8b5e33cb80cb81b65
louisdebruijn <l.e.d.de.bruijn@student.rug.nl>
2020-06-01 10:00:15 +0200

    standard format for Python scripts including Sphinx docstring

---
+++ Python tips/format.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# File name: format.py
+# Description: Basic format for Python scripts
+# Author: Louis de Bruijn
+# Date: 19-05-2020
+
+import sys
+import argparse
+import logging
+from logging import critical, error, info, warning, debug
+
+
+def parse_arguments():
+    """Read arguments from a command line."""
+    parser = argparse.ArgumentParser(description='Arguments get parsed via --commands')
+    parser.add_argument('-v', metavar='verbosity', type=int, default=2,
+        help='Verbosity of logging: 0 -critical, 1- error, 2 -warning, 3 -info, 4 -debug')
+
+    args = parser.parse_args()
+    verbose = {0: logging.CRITICAL, 1: logging.ERROR, 2: logging.WARNING, 3: logging.INFO, 4: logging.DEBUG}
+    logging.basicConfig(format='%(message)s', level=verbose[args.v], stream=sys.stdout)
+
+    return args
+
+
+def modular_function():
+    """< Summary. >
+
+    :param <variable_name>: <variable_description>, defaults to <default_value>
+    :type <variable_name>: <variable_type>(, optional)
+    <other parameters and types>
+    :raises <error_type>: <error_description>
+    <other exceptions>
+    :rtype: <return_type>
+    :return: <return_description>
+    """
+    return "This modular function with examplary Sphinx docstring conventions."
+
+
+def main():
+
+    info("Running the main program.")
+
+    print(modular_function)
+
+    try:
+        'string' + 10
+    except Exception as e:
+        error(str(e))
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main()
