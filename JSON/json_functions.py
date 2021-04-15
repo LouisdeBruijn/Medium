4f94b222b4ad3df742909341324e0cee59db1648
LED de Bruijn <louisemiledebruijn@gmail.com>
2021-04-15 10:46:16 +0200

    added unescape_html and wrapper for escaping HTML

--- JSON/json_functions.py
+++ JSON/json_functions.py
@@ -1,4 +1,6 @@
 import json
+import html
+
 
 def is_json(string):
     """Check if string is valid JSON."""
@@ -31,3 +33,40 @@ def write_jsonl(sentences, file_loc=False):
     """
     with jsonlines.open(file_loc, 'w') as writer:
         writer.write_all(sentences)
+     
+
+def unescape_html(
+        text: str) -> str:
+    """Converts any HTML entities found in text to their textual representation.
+    :param text: utterance that may contain HTML entities
+    :type text: str
+    Example of HTML entities found during annotations
+        html_entities = [("&nbsp;", " ")
+            , ("&amp;", "&")
+            , ("&gt;", ">")
+            , ("&lt;", "<")
+            , ("&le;", "≤")
+            , ("&ge;", "≥")]
+    :return: utterance wihtout HTML entities
+    :rtype: str
+    """
+    return html.unescape(text)
+
+
+def unescape_html_wrapper(json_data):
+    """Escapes HTML entities from strings in data to be saved in JSON format.
+    :param json_data: The data that is going to be saved in JSON format.
+    :type json_data: list
+    :return: The data with HTML entities unescaped
+    :rtype: list
+    """
+    if isinstance(json_data, list):
+        for idx, elem in enumerate(json_data):
+            if isinstance(elem, str):
+                json_data[idx] = unescape_html(elem)
+            elif isinstance(elem, dict):
+                for key, value in elem.items():
+                    if isinstance(value, str):
+                        elem[key] = unescape_html(value)
+
+    return json_data
