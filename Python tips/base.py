77d0ac8c1fa4db83b539c1419c09256d7ecf4f36
Louis de Bruijn <louis.de.bruijn@ing.com>
2021-05-27 10:05:16 +0200

    check

--- Python tips/base.py
+++ Python tips/base.py
@@ -1,35 +1,45 @@
 #!/usr/bin/env python3
 
-import os
-import sys
 import argparse
+import html
 import logging
-from logging import debug, info, warning, error, critical
-import json
+from typing import List
+
+import pandas as pd
 
 
 def parse_arguments():
     """Read arguments from a command line."""
-    parser = argparse.ArgumentParser(description='Arguments get parsed via --commands')
-    parser.add_argument('-v', metavar='verbosity', type=int, default=4,
-        help='Verbosity of logging: 0 -critical, 1- error, 2 -warning, 3 -info, 4 -debug')
+    parser = argparse.ArgumentParser(description="Arguments get parsed via --commands")
+    parser.add_argument(
+        "-v",
+        metavar="verbosity",
+        type=int,
+        default=4,
+        help="Verbosity of logging: 0 -critical, 1- error, 2 -warning, 3 -info, 4 -debug",
+    )
 
     args = parser.parse_args()
-    verbose = {0: logging.CRITICAL, 1: logging.ERROR, 2: logging.WARNING, 3: logging.INFO, 4: logging.DEBUG}
-    logging.basicConfig(format='%(message)s', level=verbose[args.v], filename='output/errors.log')
+    verbose = {
+        0: logging.CRITICAL,
+        1: logging.ERROR,
+        2: logging.WARNING,
+        3: logging.INFO,
+        4: logging.DEBUG,
+    }
+    logging.basicConfig(format="%(message)s", level=verbose[args.v], filename="output/errors.log")
     return args
 
 
 def print_df(df, rows, exit_script=False):
     """Prints Pandas DataFrame."""
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    with pd.option_context("display.max_rows", None, "display.max_columns", None):
         print(df.head(rows))
         if exit_script:
             exit()
 
 
-def unescape_html(
-        text: str) -> str:
+def unescape_html(text: str) -> str:
     """Converts any HTML entities found in text to their textual representation.
     :param text: utterance that may contain HTML entities
     :type text: str
@@ -46,8 +56,7 @@ def unescape_html(
     return html.unescape(text)
 
 
-def equal_array_items(
-        x: List) -> bool:
+def equal_array_items(x: List) -> bool:
     """Compares whether all array items are of the same type and content.
     Example:
         lst = [{'end': 20, 'labels': ['NORP'], 'start': 13, 'text': 'english'}
