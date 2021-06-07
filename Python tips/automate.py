9988f3f09b94269da1d09881e700966cd5b14d61
Louis de Bruijn <louis.de.bruijn@ing.com>
2021-06-07 22:36:55 +0200

    automate docstrings from type-hints

--- Python tips/automate.py
+++ Python tips/automate.py
@@ -1,17 +1,18 @@
+import re
 from collections import defaultdict
 from pathlib import Path
-from typing import Union
+from typing import Union, get_type_hints
 
 
 def automate_mkdocs_from_docstring(
-    mkdocs_dir: Union[str, Path], mkgendocs_f: str, repo_dir: Union[str, Path], match_string: str
-):
-    """Automates the -pages for mkgendocs package by adding all functions in a directory.
+    mkdocs_dir: Union[str, Path], mkgendocs_f: str, repo_dir: Path, match_string: str
+) -> str:
+    """Automates the -pages for mkgendocs package by adding all Python functions in a directory to the mkgendocs config.
 
     Args:
         mkdocs_dir (str): textual directory for the hierarchical directory & navigation in Mkdocs
         mkgendocs_f (str): The configurations file for the mkgendocs package
-        repo_dir (str): textual directory to search for Python functions in
+        repo_dir (Path): textual directory to search for Python functions in
         match_string (str): the text to be matches, after which the functions will be added in mkgendocs format
 
     Example:
@@ -33,7 +34,6 @@ def automate_mkdocs_from_docstring(
 
             for line in lines:
                 if line.startswith("def ") and "main()" not in line:
-                    import re
 
                     function = re.split("def |\\(", line)[1]
                     functions[script].append(function)
@@ -71,7 +71,149 @@ def automate_mkdocs_from_docstring(
     return f"Added to {mkgendocs_f}: {tuple(functions.values())}."
 
 
+def indent(string: str) -> int:
+    """
+
+    Args:
+        string (str): str
+
+    Returns:
+        int: Number of whitespace indentations
+
+    """
+    return sum(4 if char == "\t" else 1 for char in string[: -len(string.lstrip())])
+
+
 def main():
+
+    repo_dir = Path("/Users/hd65ox/Repositories/Medium/Python tips")
+
+    p = repo_dir.glob("**/*.py")
+    scripts = [x for x in p if x.is_file()]
+
+    functions = defaultdict(list)
+    for script in scripts:
+
+        new_arguments = {}
+        with open(script, "r") as file:
+            script_lines = file.readlines()
+
+            import importlib
+
+            module = importlib.import_module(script.stem)
+
+            for idx, line in enumerate(script_lines):
+                if line.startswith("def ") and "main()" not in line:
+
+                    function = re.split("def |\\(", line)[1]
+                    functions[script].append(function)
+
+                    f_ = getattr(module, function)
+                    type_hints = get_type_hints(f_)
+
+                    return_hint = type_hints.pop("return", None)
+                    if type_hints:
+
+                        file = "".join(script_lines[idx:])
+                        raw_docstring = re.search(r'((?:"){1,3})([^"]+)\1', file)
+
+                        if raw_docstring:
+
+                            docstring_content = raw_docstring.group()
+                            # docstring_lines = docstring_content.split("\n")
+
+                            args = re.search(
+                                r'    Args:(.*?)(    Example[s]?:|    Return[s]?:|    """)',
+                                docstring_content,
+                                re.DOTALL,
+                            )
+
+                            if args:
+
+                                arguments = args.group()
+                                argument_lines = arguments.split("\n")
+
+                                exclude = ["Args:", "Example:", "Examples:", "Returns:", '"""']
+
+                                argument_lines = [arg for arg in argument_lines if arg]
+                                argument_lines = [arg for arg in argument_lines if not any(x in arg for x in exclude)]
+
+                                for argument in argument_lines:
+                                    arg_name = argument.split()[0]
+                                    if arg_name in argument:
+
+                                        if argument.split(":"):
+                                            if "(" and ")" in argument.split(":")[0]:
+                                                new_argument_docstring = re.sub(
+                                                    r"\(.*?\)", f"({str(type_hints[arg_name])})", argument
+                                                )
+
+                                                idx = script_lines.index(f"{argument}\n")
+                                                new_arguments[idx] = f"{new_argument_docstring}\n"
+
+                                            else:
+                                                print(f"no variable type in the argument: {arg_name}")
+                                        else:
+                                            print(f"no 'arg : description'-format for this argument: {arg_name}")
+                                    else:
+                                        print(f"no docstring for this argument: {arg_name}")
+                            else:
+                                print(f"there are no arguments in this docstring: {function}")
+                        else:
+                            print(f"no docstring for function: {function}")
+
+                        if return_hint:
+
+                            raw_return = re.search(r'    Return[s]?:(.*?)(    """)', docstring_content, re.DOTALL)
+
+                            if raw_return:
+
+                                return_argument = raw_return.group()
+                                return_lines = return_argument.split("\n")
+
+                                exclude = ["Returns:", '"""']
+
+                                return_lines = [return_arg for return_arg in return_lines if return_arg]
+                                return_lines = [
+                                    return_arg
+                                    for return_arg in return_lines
+                                    if not any(x in return_arg for x in exclude)
+                                ]
+
+                                if return_lines and len(return_lines) == 1:
+
+                                    return_arg = return_lines[0]
+                                    if return_arg.split(":"):
+
+                                        new_return_docstring = re.sub(r"\w(.*:)", f"{str(return_hint)}:", return_arg)
+
+                                        idx = script_lines.index(f"{return_arg}\n")
+                                        new_arguments[idx] = f"{new_return_docstring}\n"
+
+                                    else:
+                                        print(f"no variable-type in return statement docstring: {function}")
+                                else:
+                                    print(f"no return statement docstring argument: {function}")
+
+                            else:
+                                print(f"no return argument in docstring for function: {function}")
+
+                        else:
+                            print(f"no return type-hint for function: {function}")
+
+                    else:
+                        print(f"no type-hints for function: {function}")
+
+        sorted_arguments = sorted(new_arguments.items(), reverse=True)
+
+        for (idx, new_arg) in sorted_arguments:
+            script_lines[idx] = new_arg
+
+        with open(f"{repo_dir}/test_it.py", "w") as overwrite_script:
+            overwrite_script.writelines(script_lines)
+        exit()
+
+    exit()
 
     python_tips_dir = (
         Path.cwd()
--- docs/scripts/Python tips/automate.md
+++ docs/scripts/Python tips/automate.md
@@ -2,23 +2,23 @@
 
 
 ### automate_mkdocs_from_docstring
-[source](https://github.com/LouisdeBruijn/Medium/blob/master/Python tips/automate.py/#L6)
+[source](https://github.com/LouisdeBruijn/Medium/blob/master/Python tips/automate.py/#L7)
 ```python
 .automate_mkdocs_from_docstring(
-   mkdocs_dir: Union[str, Path], mkgendocs_f: str, repo_dir: Union[str, Path],
+   mkdocs_dir: Union[str, Path], mkgendocs_f: str, repo_dir: Path,
    match_string: str
 )
 ```
 
 ---
-Automates the -pages for mkgendocs package by adding all functions in a directory.
+Automates the -pages for mkgendocs package by adding all Python functions in a directory to the mkgendocs config.
 
 
 **Args**
 
 * **mkdocs_dir** (str) : textual directory for the hierarchical directory & navigation in Mkdocs
 * **mkgendocs_f** (str) : The configurations file for the mkgendocs package
-* **repo_dir** (str) : textual directory to search for Python functions in
+* **repo_dir** (Path) : textual directory to search for Python functions in
 * **match_string** (str) : the text to be matches, after which the functions will be added in mkgendocs format
 
 
