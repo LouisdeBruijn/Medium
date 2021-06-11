f584f6348951d3d542b833ba53128329cfa78709
Louis de Bruijn <louis.de.bruijn@ing.com>
2021-06-11 16:09:57 +0200

    new automation functionality added

--- Python tips/automate.py
+++ Python tips/automate.py
@@ -12,17 +12,17 @@ def automate_mkdocs_from_docstring(
     mkdocs_dir: Union[str, Path], mkgendocs_f: str, repo_dir: Path, match_string: str
 ) -> str:
     """Automates the -pages for mkgendocs package by adding all Python functions in a directory to the mkgendocs config.
-
+    
     Args:
         mkdocs_dir (typing.Union[str, pathlib.Path]): textual directory for the hierarchical directory & navigation in Mkdocs
         mkgendocs_f (str): The configurations file for the mkgendocs package
         repo_dir (pathlib.Path): textual directory to search for Python functions in
         match_string (str): the text to be matches, after which the functions will be added in mkgendocs format
-
+    
     Example:
         >>>
         >>> automate_mkdocs_from_docstring('scripts', repo_dir=Path.cwd(), match_string='pages:')
-
+    
     Returns:
         str: feedback message
 
@@ -79,10 +79,10 @@ def automate_mkdocs_from_docstring(
 
 def indent(string: str) -> int:
     """Count the indentation in whitespace characters.
-
+    
     Args:
         string (str): str
-
+    
     Returns:
         int: Number of whitespace indentations
 
@@ -92,11 +92,11 @@ def indent(string: str) -> int:
 
 def docstring_from_type_hints(repo_dir: Path, overwrite_script: bool = False) -> str:
     """Automate docstring argument variable-type from type-hints.
-
+    
     Args:
         repo_dir (pathlib.Path): textual directory to search for Python functions in
         overwrite_script (bool): enables automatic overwriting of Python scripts in repo_dir
-
+    
     Returns:
         str: feedback message
 
@@ -293,7 +293,7 @@ def main():
     # python_tips_dir = Path.cwd().joinpath("Medium/Python tips")
     python_tips_dir = Path.cwd().joinpath("Python tips")
 
-    # docstring_from_type_hints(python_tips_dir, overwrite_script=True)
+    docstring_from_type_hints(python_tips_dir, overwrite_script=True)
 
     automate_mkdocs_from_docstring(
         mkdocs_dir="scripts",
