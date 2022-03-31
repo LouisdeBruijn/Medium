d5902e6135f9592d1094b37459a10cbd9544e4bf
Louis de Bruijn <louis.de.bruijn@ing.com>
2022-03-31 16:44:53 +0200

    new tips

--- Python tips/base.py
+++ Python tips/base.py
@@ -10,30 +10,38 @@ import pandas as pd
 
 def parse_arguments():
     """Read arguments from a command line."""
-    parser = argparse.ArgumentParser(description="Arguments get parsed via --commands")
+    parser = argparse.ArgumentParser(description='Arguments get parsed via --commands')
     parser.add_argument(
-        "-v",
-        metavar="verbosity",
+        '-v',
+        metavar='verbosity',
         type=int,
         default=4,
-        help="Verbosity of logging: 0 -critical, 1- error, 2 -warning, 3 -info, 4 -debug",
+        help='Verbosity of logging: 0 -critical, 1- error, 2 -warning, 3 -info, 4 -debug',
     )
 
     args = parser.parse_args()
-    verbose = {0: logging.CRITICAL, 1: logging.ERROR, 2: logging.WARNING, 3: logging.INFO, 4: logging.DEBUG}
-    logging.basicConfig(format="%(message)s", level=verbose[args.v], filename="output/errors.log")
+    verbose = {
+        0: logging.CRITICAL,
+        1: logging.ERROR,
+        2: logging.WARNING,
+        3: logging.INFO,
+        4: logging.DEBUG,
+    }
+    logging.basicConfig(
+        format='%(message)s', level=verbose[args.v], filename='output/errors.log'
+    )
     return args
 
 
 def print_df(df: pd.DataFrame, rows: int, exit_script: bool = False) -> None:
     """Prints a DataFrame.
-    
+
     Args:
         df (pandas.core.frame.DataFrame): tabular view to print.
         rows (int): the number of rows to print.
         exit_script (bool): whether to exit the script.
     """
-    with pd.option_context("display.max_rows", None, "display.max_columns", None):
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         print(df.head(rows))
         if exit_script:
             exit()
@@ -41,10 +49,10 @@ def print_df(df: pd.DataFrame, rows: int, exit_script: bool = False) -> None:
 
 def unescape_html(text: str) -> str:
     """Converts any HTML entities found in text to their textual representation.
-    
+
     Args:
         text (str): utterance that may contain HTML entities.
-    
+
     Examples:
         >>>
         >>> unescape_html("&nbsp;")
@@ -59,7 +67,7 @@ def unescape_html(text: str) -> str:
         "≤"
         >>> unescape_html("&ge;")
         "≥"
-    
+
     Returns:
         str: utterance without HTML entities.
 
@@ -69,17 +77,17 @@ def unescape_html(text: str) -> str:
 
 def equal_array_items(x: List) -> bool:
     """Compares whether all array items are of the same type and content.
-    
+
     Args:
         x (typing.List): an array to compare list items in.
-    
+
     Examples:
         >>>
         >>> equal_array_items(["1", "1"])
         True
         >>> equal_array_items(["1", "2"])
         False
-    
+
     Returns:
         bool: True if all items in this list are equal, False otherwise.
 
